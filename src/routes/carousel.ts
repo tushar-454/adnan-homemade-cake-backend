89567faa7a8f2ce01e810c3e11fd9140d278e2c7
tushar-454 <imtushar454@gmail.com>
2025-01-11 17:23:28 +0600

    implement carousel deletion: add deleteCarousel controller and update routes

--- src/routes/carousel.ts
+++ src/routes/carousel.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { createCarousel } from '../controllers/carousel/create_carousel';
+import { deleteCarousel } from '../controllers/carousel/delete_carousel';
 import { getCarousel } from '../controllers/carousel/get_carousel';
 import { createCarouselValidation } from '../validation/carousel/create_carousel';
 
@@ -7,5 +8,6 @@ const router = Router();
 
 router.post('/', createCarouselValidation, createCarousel);
 router.get('/', getCarousel);
+router.delete('/:carouselId', deleteCarousel);
 
 export { router as carouselRoutes };
