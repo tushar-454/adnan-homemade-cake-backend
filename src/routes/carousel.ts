2808c5b5d048c863ef0648aa22db37cb55accc77
tushar-454 <imtushar454@gmail.com>
2025-01-11 17:20:43 +0600

    implement carousel retrieval: add getCarousel controller and service, update routes

--- src/routes/carousel.ts
+++ src/routes/carousel.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { createCarousel } from '../controllers/carousel/create_carousel';
+import { getCarousel } from '../controllers/carousel/get_carousel';
 import { createCarouselValidation } from '../validation/carousel/create_carousel';
 
 const router = Router();
 
 router.post('/', createCarouselValidation, createCarousel);
+router.get('/', getCarousel);
 
 export { router as carouselRoutes };
---
+++ src/services/carousel/get_carousel.ts
@@ -0,0 +1,15 @@
+import { Carousel, ICarousel } from '../../models/Carousel';
+
+const getCarouselService = async (): Promise<ICarousel[] | undefined> => {
+  try {
+    const carousel = await Carousel.find();
+    return carousel || [];
+  } catch (error) {
+    console.log('Error in getCarouselService', error);
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+};
+
+export { getCarouselService };
