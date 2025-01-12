0db34941f3b8168b4f75fa26317d4e078497d7a8
tushar-454 <imtushar454@gmail.com>
2025-01-12 23:36:53 +0600

    implement gallery retrieval: add getGallery controller, service, and update routes

--- src/routes/gallery.ts
+++ src/routes/gallery.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { createGallery } from '../controllers/gallery/create_gallery';
+import { getGallery } from '../controllers/gallery/get_gallery';
 import { createGalleryValidation } from '../validation/gallery/create_gallery';
 
 const router = Router();
 
 router.post('/', createGalleryValidation, createGallery);
+router.get('/', getGallery);
 
 export { router as galleryRoutes };
