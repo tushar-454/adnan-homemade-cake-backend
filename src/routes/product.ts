65547b1706cb43a7f6727554901d973288239a18
tushar-454 <imtushar454@gmail.com>
2025-01-13 17:54:19 +0600

    implement product deletion: add deleteProduct controller and route

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { createProduct } from '../controllers/product/create_product';
+import { deleteProduct } from '../controllers/product/delete_product';
 import { getProduct } from '../controllers/product/get_product';
 import { updateProduct } from '../controllers/product/update_product';
 import { createProductValidation } from '../validation/product/create_product';
@@ -10,5 +11,6 @@ const router = Router();
 router.post('/', createProductValidation, createProduct);
 router.get('/', getProduct);
 router.put('/:productId', updateProductValidation, updateProduct);
+router.delete('/:productId', deleteProduct);
 
 export { router as productRoutes };
