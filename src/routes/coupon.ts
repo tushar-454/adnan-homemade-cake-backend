a56d7d792af4baa645bd431abb89ab79e7fc0791
tushar-454 <imtushar454@gmail.com>
2025-01-21 01:17:16 +0600

    implement get coupon by code functionality: add getCouponByCode controller and route for retrieving coupons by their code

--- src/routes/coupon.ts
+++ src/routes/coupon.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { createCoupon } from '../controllers/coupon/create_coupon';
 import { deleteCoupon } from '../controllers/coupon/delete_coupon';
 import { getCoupon } from '../controllers/coupon/get_coupon';
+import { getCouponByCode } from '../controllers/coupon/get_coupon_by_code';
 import { updateCoupon } from '../controllers/coupon/update_coupon';
 import { createCouponValidation } from '../validation/coupon/create_coupon';
 import { updateCouponValidation } from '../validation/coupon/update_coupon';
@@ -10,6 +11,7 @@ const router = Router();
 
 router.post('/', createCouponValidation, createCoupon);
 router.get('/', getCoupon);
+router.get('/:code', getCouponByCode);
 router.put('/:couponId', updateCouponValidation, updateCoupon);
 router.delete('/:couponId', deleteCoupon);
 
