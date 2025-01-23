7866ecc765971ab17840ffa49cb6e75b8839325c
tushar-454 <imtushar454@gmail.com>
2025-01-23 23:20:09 +0600

    implement verify-admin middleware and add it in carousel routes

--- src/routes/carousel.ts
+++ src/routes/carousel.ts
@@ -3,14 +3,16 @@ import { createCarousel } from '../controllers/carousel/create_carousel';
 import { deleteCarousel } from '../controllers/carousel/delete_carousel';
 import { getCarousel } from '../controllers/carousel/get_carousel';
 import { updateCarousel } from '../controllers/carousel/update_carousel';
+import { verifyAdmin } from '../middlewares/verify_admin';
+import verifyToken from '../middlewares/verify_token';
 import { createCarouselValidation } from '../validation/carousel/create_carousel';
 import { updateCarouselValidation } from '../validation/carousel/update_carousel';
 
 const router = Router();
 
-router.post('/', createCarouselValidation, createCarousel);
+router.post('/', verifyToken, verifyAdmin, createCarouselValidation, createCarousel);
 router.get('/', getCarousel);
-router.put('/:carouselId', updateCarouselValidation, updateCarousel);
-router.delete('/:carouselId', deleteCarousel);
+router.put('/:carouselId', verifyToken, verifyAdmin, updateCarouselValidation, updateCarousel);
+router.delete('/:carouselId', verifyToken, verifyAdmin, deleteCarousel);
 
 export { router as carouselRoutes };
