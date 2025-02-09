a8f804327ef0bff1cf81fc3f0b21ed56855ac338
tushar-454 <imtushar454@gmail.com>
2025-02-09 17:32:42 +0600

    update get_product to make sure to filter product using min & max or min or max price

--- src/services/product/get_product.ts
+++ src/services/product/get_product.ts
@@ -1,3 +1,4 @@
+import { TQuery } from '../../controllers/product/get_product';
 import { IProduct, Product } from '../../models/Product';
 
 type IProductQuery = {
@@ -6,10 +7,11 @@ type IProductQuery = {
   sell_count?: { $gte: number };
   is_upcoming?: boolean;
   category?: { $in: string[] };
+  price?: { $gte?: number, $lte?: number };
 };
 
-const getProductService = async (props: Partial<IProduct>): Promise<IProduct[] | undefined> => {
-  const { is_featured, is_deleted, sell_count, is_upcoming, category } = props;
+const getProductService = async (props: Partial<IProduct> & TQuery): Promise<IProduct[] | undefined> => {
+  const { is_featured, is_deleted, sell_count, is_upcoming, category, min_price, max_price } = props;
   const categories = category?.split(',');
   try {
     const query: IProductQuery = {};
@@ -18,6 +20,15 @@ const getProductService = async (props: Partial<IProduct>): Promise<IProduct[] |
     if (sell_count) query.sell_count = { $gte: +sell_count };
     if (is_upcoming) query.is_upcoming = is_upcoming;
     if (category && Array.isArray(categories)) query.category = { $in: categories};
+    const convert_min_price = Number(min_price || 0);
+    const convert_max_price = Number(max_price || 0);
+    if (convert_min_price && convert_max_price) {
+      query.price = { $gte: convert_min_price, $lte: convert_max_price };
+    } else if (convert_min_price) {
+      query.price = { $gte: convert_min_price }; 
+    } else if (convert_max_price) {
+      query.price = { $lte: convert_max_price }; 
+    }
 
     const products = await Product.find(query as any)
       .sort({ createdAt: -1 })
